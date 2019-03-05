8220d1cd89ad8bc7b26f8d71733942656aebe106
rv98 <31803660+rv98@users.noreply.github.com>
2019-03-05 18:24:38 +0530

    V2 - html page to display data
    added home.html to display data on home

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import user
 # Create your views here.
+def home(request):
+	data=user.objects.all();
+	return render(request,"myapp/home.html",{"data":data})
