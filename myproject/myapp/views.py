633aacaa4de8233dff1fa8c7c46815f98c85dc82
rv98 <31803660+rv98@users.noreply.github.com>
2019-03-05 18:50:17 +0530

    V3 - Login Module
    Added a login module

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 from .models import user
+from .forms import addUser
 # Create your views here.
 def home(request):
 	data=user.objects.all();
 	return render(request,"myapp/home.html",{"data":data})
+
+def login(request):
+	form = addUser();
+	return render(request,"myapp/login.html",{'form':form})
+
+def index(request):
+	name=request.GET.get("name")
+	password=request.GET.get("password")
+
+	data=user.objects.all();
+
+	for d in data:
+		if d.name==name:
+			if d.password==password:
+				return render(request,"myapp/index.html",{'uname':name})
+		else:
+			return render(request,"myapp/index.html",{'uname':'error'})
+
+	
